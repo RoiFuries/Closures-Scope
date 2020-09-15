de124aa4703ad46999efeac4fb688606db29dae5
RoiFuries <roi.furies@gmail.com>
2020-09-15 00:26:36 -0500

    scopes

---
+++ src/Scope/global.js
@@ -0,0 +1,18 @@
+var hello ='hellow'
+var hello = 'jojojojk'
+let world = "world"
+// let world = "esto es un error"
+const hola = 'hola mundo'
+
+const anotherFunction = () => {
+    console.log(hello);
+    console.log(world);
+    console.log(hola);
+}
+anotherFunction()
+
+const macormic = () => {
+    globalVar = 'im global';    
+}
+macormic()
+console.log(globalVar);
